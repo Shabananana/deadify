aa686b690f410f6f56ffc4d9c51682407bf03ae5
Jeffrey Davis <jeff.davis@karmaloop.com>
2013-06-02 17:28:56 -0400

    Initial work on deadify, added test assets

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject deadify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A utility program to identify dead css selectors and html class/id attributes."
+  :url "https://github.com/Shabananana/deadify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
--- src/deadify/core.clj
+++ src/deadify/core.clj
@@ -1,9 +1,23 @@
 (ns deadify.core
-  (:gen-class))
+  (:gen-class)
+  (:import java.io.File)
+  (:import java.io.FileNotFoundException))
+
+
+(def view-root-directory "/")
+(def css-root-directory "./test/deadify/test-assets/stylesheets")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (file-seq (File. "."))
+  (doseq [file (file-seq (File. css-root-directory))]
+    (println file)))
+
+
+(defn get-files [x]
+  (file-seq (File. x))
+  (doseq [file (file-seq (File. x))]
+    (println file)))
