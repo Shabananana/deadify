0ddde5fccd501a0726631fbf0f3e74ede60cc052
Jeffrey Davis <jeff.davis@karmaloop.com>
2013-06-02 21:13:05 -0400

    Continued work on deadify; added function to read file by line

--- src/deadify/core.clj
+++ src/deadify/core.clj
@@ -4,20 +4,26 @@
   (:import java.io.FileNotFoundException))
 
 
-(def view-root-directory "/")
+(def view-root-directory "./test/deadify/test-assets/Views")
 (def css-root-directory "./test/deadify/test-assets/stylesheets")
+
+(defn get-files [x]
+  (file-seq (File. x))
+  (doseq [file (file-seq (File. x))]
+    (println (clojure.string/replace file #"[\\]" #(str %1 %1)))))
+
+(defn parse-css [x]
+  )
+
+(defn read-file-by-line [x]
+  (with-open [rdr (java.io.BufferedReader. (java.io.FileReader. x))]
+  (let [seq (line-seq rdr)]
+    (count seq))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (file-seq (File. "."))
-  (doseq [file (file-seq (File. css-root-directory))]
-    (println file)))
-
-
-(defn get-files [x]
-  (file-seq (File. x))
-  (doseq [file (file-seq (File. x))]
-    (println file)))
+  (get-files css-root-directory)
+  (get-files view-root-directory))
