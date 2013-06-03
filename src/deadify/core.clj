a9fddbbd5704f25d65f2c84a551bd58b78f8be04
Jeffrey Davis <jeff.davis@karmaloop.com>
2013-06-02 22:12:47 -0400

    Continued work

--- src/deadify/core.clj
+++ src/deadify/core.clj
@@ -3,22 +3,28 @@
   (:import java.io.File)
   (:import java.io.FileNotFoundException))
 
-
 (def view-root-directory "./test/deadify/test-assets/Views")
 (def css-root-directory "./test/deadify/test-assets/stylesheets")
+
+(def not-nil? (complement nil?))
+
+(defn parse-css [x coll]
+  ((if (not-nil? (re-find #"[(\w+)?(\s*>\s*)?(#\w+)?\s*(\.\w+)?\s*]" x))
+    (conj coll x)
+    (println x)))
+
+(defn read-file-by-line [x]
+  (if (.isFile? (file-seq (File. x)))
+    (with-open [rdr (java.io.BufferedReader. (java.io.FileReader. x))]
+    (let [seq (line-seq rdr)]
+      (count seq)
+      (parse-css x nil)))))
 
 (defn get-files [x]
   (file-seq (File. x))
   (doseq [file (file-seq (File. x))]
-    (println (clojure.string/replace file #"[\\]" #(str %1 %1)))))
-
-(defn parse-css [x]
-  )
-
-(defn read-file-by-line [x]
-  (with-open [rdr (java.io.BufferedReader. (java.io.FileReader. x))]
-  (let [seq (line-seq rdr)]
-    (count seq))))
+    (println (clojure.string/replace file #"[\\]" #(str %1 %1)))
+    (read-file-by-line (clojure.string/replace file #"[\\]" #(str %1 %1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
